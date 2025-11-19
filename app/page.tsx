63d0fa152814fc7c3a2dc354ba1eabea6831a972
Joseph Gaither <joe@joepro.ai>
2025-11-18 23:58:47 -0500

    MAKE NEURAL NETWORK IMPOSSIBLE TO MISS: 40px dots with massive glow, 20 dots total

--- app/page.tsx
+++ app/page.tsx
@@ -15,6 +15,16 @@ export default function Home() {
         <div className="neural-dot"></div>
         <div className="neural-dot"></div>
         <div className="neural-dot"></div>
+        <div className="neural-dot"></div>
+        <div className="neural-dot"></div>
+        <div className="neural-dot"></div>
+        <div className="neural-dot"></div>
+        <div className="neural-dot"></div>
+        <div className="neural-dot"></div>
+        <div className="neural-dot"></div>
+        <div className="neural-dot"></div>
+        <div className="neural-dot"></div>
+        <div className="neural-dot"></div>
         <div className="neural-scan"></div>
       </div>
       
